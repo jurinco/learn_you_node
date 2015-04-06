5b7322fbbe71d631a17f21d2f8ca7c061656dbc6
Nick Slocum <nslocum@gmail.com>
2015-04-06 14:56:34 -0400

    Finished 5.

---
+++ 5_filtered_ls.js
@@ -0,0 +1,30 @@
+// Create a program that prints a list of files in a given directory, filtered
+// by the extension of the files. You will be provided a directory name as the
+// first argument to your program (e.g. '/path/to/dir/') and a file extension to
+// filter by as the second argument.
+
+// For example, if you get 'txt' as the second argument then you will need to
+// filter the list to only files that end with .txt. Note that the second
+// argument will not come prefixed with a '.'.
+
+// The list of files should be printed to the console, one file per line. You
+// must use asynchronous I/O.
+
+var fs = require('fs')
+var path = require('path')
+
+var dir = process.argv[2]
+var ext = process.argv[3]
+
+var hasFileExt = function (filename) {
+  return path.extname(filename) === '.' + ext
+}
+
+fs.readdir(dir, function (err, files) {
+  if (err) throw err;
+  files.forEach(function (file) {
+    if (hasFileExt(file)) {
+      console.log(file)
+    }
+  })
+})
